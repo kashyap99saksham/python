981cdd4e39c381a3218b7f127574a07cd811111d
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2019-06-04 23:55:51 +0530

    Add files via upload

---
+++ chapter15.py
@@ -0,0 +1,18 @@
+# INTRODUCTION OF GENERATORS : ARE ITERATORS
+# ITERABLE = [1,2,3,4,5]
+# ITERATOR = [1] THEN [2] THEN [3] THEN [4] THEN [5]
+
+# generator function = yield ()
+
+def fun(n):
+    for i in range(1,n+1):
+        yield(i)        #it makes a sequence(LESS MEMORY||FAST)
+print(fun(4))
+for i in fun(4):
+    print(i)    #1 2 3 4
+nums = fun(4)
+for i in nums:
+    print(i)
+for i in nums:      #IT CANNOT RUN AGAIN : BECOZ IT REMOVE PREVIOUS VALUE
+    print(i)
+    
